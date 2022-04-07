e7b3baa1f0c497dd1e5217f3197d34424379dd52
EC2 Default User <ec2-user@ip-172-31-84-67.ec2.internal>
2022-04-07 06:47:42 +0000

    [Add]コメント機能の非同期通信

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,17 +1,26 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
+    @book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = @book.id
+    if @comment.save
+      flash.now[:notice] = 'コメントを投稿しました'
+        #render先にjsファイルを指定
+    else
+      render :books/show
     # コメント後は行う前の画面に遷移すること
-    redirect_to request.referer
+    # redirect_to request.referer
+    end
   end
 
   def destroy
     BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
     # コメント削除後は行う前の画面に遷移すること
-    redirect_to request.referer
+    # redirect_to request.referer
+    flash.now[:alert] = '投稿を削除しました'
+    #renderしたときに@postのデータがないので@postを定義
+    @post = Post.find(params[:post_id])
+    #render先にjsファイルを指定
   end
 
   private
