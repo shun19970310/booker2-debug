ce69655aeb87eccd17e994dd2bddfc7de3a9d060
EC2 Default User <ec2-user@ip-172-31-84-67.ec2.internal>
2022-04-07 08:17:20 +0000

    [Finish]応用課題6と不明箇所の解消

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,26 +1,19 @@
 class BookCommentsController < ApplicationController
   def create
-    @book = Book.find(params[:book_id])
+    book = Book.find(params[:book_id])
     @comment = current_user.book_comments.new(book_comment_params)
-    @comment.book_id = @book.id
+    @comment.book_id = book.id
     if @comment.save
       flash.now[:notice] = 'コメントを投稿しました'
-        #render先にjsファイルを指定
-    else
-      render :books/show
-    # コメント後は行う前の画面に遷移すること
-    # redirect_to request.referer
     end
   end
 
   def destroy
-    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
+    @comment = BookComment.find_by(id: params[:id], book_id: params[:book_id])
+    @comment.destroy
     # コメント削除後は行う前の画面に遷移すること
     # redirect_to request.referer
     flash.now[:alert] = '投稿を削除しました'
-    #renderしたときに@postのデータがないので@postを定義
-    @post = Post.find(params[:post_id])
-    #render先にjsファイルを指定
   end
 
   private
